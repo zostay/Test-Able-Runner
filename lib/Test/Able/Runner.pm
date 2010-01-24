c192f7c4659264d2d603f516e6c8cdd78c6081ea
Sterling Hanenkamp <hanenkamp@cpan.org>
2010-01-23 22:54:15 -0600

    Adding the LICENSE

--- lib/Test/Able/Runner.pm
+++ lib/Test/Able/Runner.pm
@@ -155,4 +155,17 @@ sub run {
     $runner->meta->run_tests;
 }
 
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp C<< <hanenkamp@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 Qubling Software LLC.
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 1;
